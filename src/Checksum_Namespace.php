ce1ffd0327ddd2210e0fdb42be7bab36f26d8733
Alain Schlesser <alain.schlesser@gmail.com>
2017-11-22 20:44:36 +0100

    Add checksum namespace.

---
+++ src/Checksum_Namespace.php
@@ -0,0 +1,10 @@
+<?php
+
+use WP_CLI\Dispatcher\CommandNamespace;
+
+/**
+ * Verifies file integrity by comparing to published checksums.
+ */
+class Checksum_Namespace extends CommandNamespace {
+
+}
