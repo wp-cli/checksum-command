49c06c6b43657e672e8a83ef5e6fd669a6e3577e
Alain Schlesser <alain.schlesser@gmail.com>
2017-11-29 00:29:52 +0100

    Scaffold the remaining logic.

--- src/Checksum_Plugin_Command.php
+++ src/Checksum_Plugin_Command.php
@@ -40,7 +40,7 @@ class Checksum_Plugin_Command extends Checksum_Base_Command {
 		$plugins = $fetcher->get_many( $args );
 		$all     = \WP_CLI\Utils\get_flag_value( $assoc_args, 'all', false );
 
-		$has_errors = false;
+		$errors = array();
 
 		foreach ( $plugins as $plugin ) {
 			$version = $this->get_plugin_version( $plugin->file );
@@ -51,11 +51,17 @@ class Checksum_Plugin_Command extends Checksum_Base_Command {
 
 			$checksums = $this->get_plugin_checksums( $plugin->name, $version );
 
-			//  - Iterate over plugin's files
-			//     - Verify plugin file checksum against downloaded checksum
+			$files = $this->get_plugin_files( $plugin->file );
+
+			foreach ( $files as $file ) {
+				$result = $this->check_file( $file, $checksums );
+				if ( true !== $result ) {
+					$errors[ $plugin ][ $file ] = $result;
+				}
+			}
 		}
 
-		if ( ! $has_errors ) {
+		if ( empty( $errors ) ) {
 			WP_CLI::success(
 				count( $plugins ) > 1
 					? 'Plugins verify against checksums.'
@@ -132,5 +138,32 @@ class Checksum_Plugin_Command extends Checksum_Base_Command {
 		}
 
 		return $body['files'];
+	}
+
+	/**
+	 * Get the list of files that are part of the given plugin.
+	 *
+	 * @param string $path Relative path to the main plugin file.
+	 *
+	 * @return array<string> Array of files with their relative paths.
+	 */
+	private function get_plugin_files( $path ) {
+		// TODO: Fetch and return the lift of plugin files.
+		return array();
+	}
+
+	/**
+	 * Check the integrity of a single plugin file by comparing it to the
+	 * officially provided checksum.
+	 *
+	 * @param string $path      Relative path to the plugin file to check the
+	 *                          integrity of.
+	 * @param array  $checksums Array of provided checksums to compare against.
+	 *
+	 * @return true|string
+	 */
+	private function check_file( $path, $checksums ) {
+		// TODO: Compute the checksum and compare it to the provided one.
+		return true;
 	}
 }
