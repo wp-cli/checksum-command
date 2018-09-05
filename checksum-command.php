76289ab1ed460e0a5229f6814bc072a7f0e026bf
Alain Schlesser <alain.schlesser@gmail.com>
2018-09-05 08:24:12 +0200

    Use command namespaces instead of conditional addition

--- checksum-command.php
+++ checksum-command.php
@@ -9,10 +9,9 @@ if ( file_exists( $autoload ) ) {
 	require_once $autoload;
 }
 
-WP_CLI::add_hook( 'after_add_command:core', function () {
-	WP_CLI::add_command( 'core verify-checksums', 'Checksum_Core_Command' );
-} );
+WP_CLI::add_command( 'core', 'Core_Command_Namespace' );
+WP_CLI::add_command( 'core verify-checksums', 'Checksum_Core_Command' );
 
-WP_CLI::add_hook( 'after_add_command:plugin', function () {
-	WP_CLI::add_command( 'plugin verify-checksums', 'Checksum_Plugin_Command' );
-} );
+WP_CLI::add_command( 'plugin', 'Plugin_Command_Namespace' );
+WP_CLI::add_command( 'plugin verify-checksums', 'Checksum_Plugin_Command' );
+
