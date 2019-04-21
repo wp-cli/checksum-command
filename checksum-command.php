88cc22095b00f25870d0966d25cd558677fb3ebd
Thrijith Thankachan <thrijith13@gmail.com>
2019-04-21 14:22:21 +0530

    PHPCS: Update ruleset to exclude calsses from NamingConventions sniff Fix CS in checksum-command.php src/Checksum_Base_Command.php src/Checksum_Core_Command.php src/Checksum_Plugin_Command.php src/WP_CLI/Fetchers/UnfilteredPlugin.php

--- checksum-command.php
+++ checksum-command.php
@@ -4,9 +4,9 @@ if ( ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
-$autoload = dirname( __FILE__ ) . '/vendor/autoload.php';
-if ( file_exists( $autoload ) ) {
-	require_once $autoload;
+$wpcli_checksum_autoloader = dirname( __FILE__ ) . '/vendor/autoload.php';
+if ( file_exists( $wpcli_checksum_autoloader ) ) {
+	require_once $wpcli_checksum_autoloader;
 }
 
 WP_CLI::add_command( 'core', 'Core_Command_Namespace' );
